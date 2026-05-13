499f6d2816318a8c83c2da038d26b04091d1ef7d
iaadileli <type.team@grupo-sm.com>
2026-05-13 15:23:58 +0200

    Logo personal, notas por ejercicio, check auto al copiar, permitir peso corporal

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service worker mínimo para que la app funcione offline
-const CACHE = "rutina-v1";
-const ASSETS = ["./", "./index.html", "./manifest.json", "./icon.svg"];
+const CACHE = "rutina-v2";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./logo.png", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
@@ -8,7 +8,11 @@ self.addEventListener("install", e => {
 });
 
 self.addEventListener("activate", e => {
-  e.waitUntil(self.clients.claim());
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener("fetch", e => {
